ccdb216d91be0b5ab02377067b169c55ed740511
Vahlee <violetiruoghene@gmail.com>
2023-07-03 10:32:35 -0700

    strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  * _strspn - Entry point
@@ -7,25 +7,24 @@ nclude "main.h"
  * Return: Always 0 (Success)
  */
 
-
 unsigned int _strspn(char *s, char *accept)
 
 {
 	unsigned int n = 0;
+	int r;
 
-		        int r;
-			while (*s)
-				{
-					for(r = 0; accept[r]; r++)
-					{
-						if (*s == accept[r])
-						{
-							n++;
-							break;
-						}
-						else if (accept[r + 1] == '\0')
-							return (n);
-					}
-					s++;
-				}			return (n);
-}
+	while (*s)
+	{
+		for (r = 0; accept[r]; r++)
+		{
+			if (*s == accept[r])
+			{
+				n++;
+				break;
+			}
+			else if (accept[r + 1] == '\0')
+				return (n);
+		}
+		s++;
+		return (n);
+	}
