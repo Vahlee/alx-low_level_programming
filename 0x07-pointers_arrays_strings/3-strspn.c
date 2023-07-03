c49de23c0a05c31ed5e84f605db0ef3374f939b0
Vahlee <violetiruoghene@gmail.com>
2023-07-03 08:07:56 -0700

    memcpy

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,25 +7,25 @@ nclude "main.h"
  * Return: Always 0 (Success)
  */
 
+
 unsigned int _strspn(char *s, char *accept)
 
 {
 	unsigned int n = 0;
-	int r;
 
-	while (*s)
-	{
-		for (r = 0; accept[r]; r++)
-		{
-			if (*s == accept[r])
-			{
-				n++;
-				break;
-			}
-			else if (accept[r + 1] == '\0')
-				return (n);
-		}
-		s++;
-	}
-	return (n);
+		        int r;
+			while (*s)
+				{
+					for(r = 0; accept[r]; r++)
+					{
+						if (*s == accept[r])
+						{
+							n++;
+							break;
+						}
+						else if (accept[r + 1] == '\0')
+							return (n);
+					}
+					s++;
+				}			return (n);
 }
