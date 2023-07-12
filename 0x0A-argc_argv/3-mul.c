df8dc2ccb8c7196a767dcde4d89e9b9dfc032295
Vahlee <violetiruoghene@gmail.com>
2023-07-12 15:03:01 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
-
 #include "main.h"
 
 /**
  * _atoi - converts a string to an integer
  * @s: string to be converted
+ *
  * Return: the int converted from the string
  */
-
 int _atoi(char *s)
-
 {
 	int i, d, n, len, f, digit;
 
@@ -19,12 +17,15 @@ int _atoi(char *s)
 	len = 0;
 	f = 0;
 	digit = 0;
+
 	while (s[len] != '\0')
 		len++;
+
 	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
 			++d;
+
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
@@ -37,20 +38,22 @@ int _atoi(char *s)
 			f = 0;
 		}
 		i++;
-		if (f == 0)
-			return (0);
-		return (n);
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
 
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
  * @argv: array of arguments
+ *
  * Return: 0 (Success), 1 (Error)
  */
-
 int main(int argc, char *argv[])
-
 {
 	int result, num1, num2;
 
@@ -59,9 +62,13 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
 	result = num1 * num2;
+
 	printf("%d\n", result);
+
 	return (0);
 }
+
